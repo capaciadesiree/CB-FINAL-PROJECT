815789fb1021e1d77773b7bb5e8807c44f4d2adc
yuliiak <98550411+ykuts@users.noreply.github.com>
2025-02-15 19:01:02 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,17 @@ const db = require('./db/db');
 const { readdirSync } = require('fs');
 const app = express();
 const MongoStore = require('connect-mongo');
-const isProduction = process.env.NODE_ENV === 'production';
+
 
 // middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-  origin: isProduction ? [
-    'https://desiree-frontend.netlify.app'] : [
-    'http://localhost:3000'
-    ], 
+  origin: ['https://desiree-frontend.netlify.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
@@ -28,18 +27,19 @@ app.use(cors({
 
 app.use(session({ 
   secret: process.env.SECRET_KEY, 
-  resave: true, // changed to "true" for debug
-  saveUninitialized: true, // changed to "true" for debug
+  resave: false, // changed to "true" for debug
+  saveUninitialized: false, // changed to "true" for debug
   store: MongoStore.create({ 
     mongoUrl: process.env.MONGO_URL,
-    ttl: 24 * 60 * 60, // 24 hours
-    collectionName: 'sessions',
+    touchAfter: 24 * 3600, // 24 hours
+  
   }),
-  cookie: { 
-    secure: isProduction, // Secure in production, false in development
-    httpOnly: true, // Temporarily set false for testing (change to true in production)
-    sameSite: isProduction ? 'None' : 'Lax', // None for production, Lax for development
-    maxAge: 24 * 60 * 60 * 1000, // Add maxAge in milliseconds
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'None',
+    maxAge: 24 * 60 * 60 * 1000,
+    path: '/
   
   },
   name: 'connect.sid' // set cookie name
